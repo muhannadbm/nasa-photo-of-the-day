24513d04b75b7dba24e7e5ff4cbcfb2692e12527
muhannadbm <mhd31145@hotmail.com>
2021-07-15 19:27:54 -0400

    styling project completed

---
+++ src/themes/index.js
@@ -0,0 +1,10 @@
+export default {
+    primary: '#1d1135',
+    darkblue: '#0B0B33',
+    pinkishred: '#ba1e68',
+    blue: '#5643fd',
+    white: '#fcfbfe',
+    gray: '#bab4c6',
+    black: '#060611',
+    
+}
